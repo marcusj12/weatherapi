cc49661f46ab7dd0683a445cc0ea8ff9165b0f14
marcusj12 <marcus.a.jennings12@gmail.com>
2022-06-24 17:52:30 -0400

    Weather API - added another get mapping request for a specific set of data from open weather api,

--- src/main/java/com/careerdevs/weatherapi/controllers/CurrentWeatherController.java
+++ src/main/java/com/careerdevs/weatherapi/controllers/CurrentWeatherController.java
@@ -20,58 +20,65 @@ public class CurrentWeatherController {
     private final String BASE_URL = "https://api.openweathermap.org/data/2.5/weather";// BaseURL used to reuse parts of URl that will remain the same
 
 
-    @GetMapping("/city/{cityName}")
-    // Want request from external api; so we use ResTemplate; add it as a parameter
-    // ResponseEntity ; a way for a generic class; allows for fine level of control for the status, data, and headers of our responses
-    public ResponseEntity<?> getCurrentWeatherByCityPV(RestTemplate restTemplate, @PathVariable String cityName) { // allows anyone to choose any given city
-// The catch block; used for error handling; handle code in between try-catch; if error occurs; executes under catch; can add multiple *error executions*? under catch block to; creating *dynamic replies*?
-        try {
-            String units = "imperial";
-            String apiKey = env.getProperty("OW_API_KEY"); // Can access any properties within application.properties without sharing when uploading code
-            String queryString = "?q=" + cityName + "&appid=" + apiKey + "&units=" + units;
-            String openWeatherURL = BASE_URL + queryString;
+//    @GetMapping("/city/{cityName}")
+//    // Want request from external api; so we use ResTemplate; add it as a parameter
+//    // ResponseEntity ; a way for a generic class; allows for fine level of control for the status, data, and headers of our responses
+//    public ResponseEntity<?> getCurrentWeatherByCityPV(RestTemplate restTemplate, @PathVariable String cityName) { // allows anyone to choose any given city
+//// The catch block; used for error handling; handle code in between try-catch; if error occurs; executes under catch; can add multiple *error executions*? under catch block to; creating *dynamic replies*?
+//        try {
+//            String units = "imperial";
+//            String apiKey = env.getProperty("OW_API_KEY"); // Can access any properties within application.properties without sharing when uploading code
+//            String queryString = "?q=" + cityName + "&appid=" + apiKey + "&units=" + units;
+//            String openWeatherURL = BASE_URL + queryString;
+//
+//            //System.out.println(openWeatherURL);
+//            CurrentWeather owRes = restTemplate.getForObject(openWeatherURL, CurrentWeather.class); // Client that allow calls to be made to weather api
+//
+//            assert owRes != null;
+////            System.out.println("City: " + openWeatherResponse.getName());
+////            System.out.println("Temp: " + openWeatherResponse.getMain().getTemp());
+////            System.out.println("Description: " + openWeatherResponse.getWeather()[0].getDescription());
+//            CurrentWeatherReport report = new CurrentWeatherReport(
+//                    owRes.getName(),
+//                    owRes.getCoord(),
+//                    owRes.getMain(),
+//                    owRes.getWeather()[0],
+//                    "imperial"
+//            );
+//            // System.out.println(owRes);
+//            System.out.println(report);
+//
+//            return ResponseEntity.ok(report);
+//            //We  run code that responds to an error exception by entering faulty input data; we use the error exception to make custom reply
+//
+//        } catch (HttpClientErrorException.NotFound e) {
+//            return ResponseEntity.status(404).body("City Not Found: " + cityName);
+//
+//
+//        } catch (Exception e) {
+//            System.out.println(e.getMessage());
+//            System.out.println(e.getClass()); // indicates what the exact exception was
+//            return ResponseEntity.internalServerError().body(e.getMessage());// we don't know the error response by default; so we create a generic exception to catch all general errors c
+//        }
+//
+//
+//
+//
 
-            //System.out.println(openWeatherURL);
-            CurrentWeather owRes = restTemplate.getForObject(openWeatherURL, CurrentWeather.class); // Client that allow calls to be made to weather api
 
-            assert owRes != null;
-//            System.out.println("City: " + openWeatherResponse.getName());
-//            System.out.println("Temp: " + openWeatherResponse.getMain().getTemp());
-//            System.out.println("Description: " + openWeatherResponse.getWeather()[0].getDescription());
-            CurrentWeatherReport report = new CurrentWeatherReport(
-                    owRes.getName(),
-                    owRes.getCoord(),
-                    owRes.getMain(),
-                    owRes.getWeather()[0],
-                    "imperial"
-            );
-           // System.out.println(owRes);
-            System.out.println(report);
+    /* Whenever want to grab more than "one" request from the client, it is best to move away from PathVariables and use RequestParameters (RequestParam)
+    The help clean up our request without showing how the request is set up.
 
-            return ResponseEntity.ok(report);
-            //We  run code that responds to an error exception by entering faulty input data; we use the error exception to make custom reply
-
-        } catch (HttpClientErrorException.NotFound e) {
-            return ResponseEntity.status(404).body("City Not Found: " + cityName);
+    @RequestParam is usually used for requesting more than one dynamic value in the url
+     api/current/?city=boston
+     */
 
 
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            System.out.println(e.getClass()); // indicates what the exact exception was
-            return ResponseEntity.internalServerError().body(e.getMessage());// we don't know the error response by default; so we create a generic exception to catch all general errors c
-        }
+// http:localhost:8000/api/current/city/boston
 
 
-    }
-}
-
-
-/* @RequestParam is usually used for requesting more than one dynamic value in the url
- api/current/?city=boston
- */
-/*
-    @GetMapping("/")
-    public ResponseEntity<?> getCurrentWeatherByCityRP (RestTemplate restTemplate, @RequestParam("city") String cityName) {
+    @GetMapping("/city")
+    public ResponseEntity<?> getCurrentWeatherByCityRequestParams(RestTemplate restTemplate, @RequestParam("city") String cityName) {
         try {
             String apiKey = env.getProperty("OW_API_KEY");
             String queryString = "?q=" + cityName + "&appid=" + apiKey + "&units=imperial";
@@ -94,5 +101,7 @@ public class CurrentWeatherController {
 
 
     }
-*/// Methods are static when you dont create instances of them
+
+}
+// Methods are static when you don't create instances of them
 
